967d38d53ee3ba9818b28e2665825e172d4cefa6
sho0621techcamp <sho6217@gmail.com>
2020-10-13 19:35:59 +0900

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   private
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '0000'
