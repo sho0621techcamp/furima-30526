994b45a9db921b7edd5ffb68cc2d0804024322fa
sho0621techcamp <sho6217@gmail.com>
2020-10-15 19:40:48 +0900

    Itemモデルにwith_options追加#2

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,38 +5,38 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :shipping_region
   belongs_to_active_hash :shipping_day
-  
-  validates :image,              presence: true
-  validates :name,               presence: true, length: {maximum: 40}
-  validates :text,               presence: true, length: {maximum: 1000}
 
-  with_options presence: true, 
-  format: {with: /\A([1-9]\d*,)*[1-9]\d*\z/,
-  numericality: {only_integer: true, 
-  greater_than_or_equal_to: 300,
-  less_than_or_equal_to: 9999999 } do 
-  validates :price
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :text, length: { maximum: 1000 }
   end
-  
-  
-  validates :user,               presence: true
-  validates :category_id,        presence: true
-  validates :status_id,          presence: true
-  validates :shipping_fee_id,    presence: true
-  validates :shipping_region_id, presence: true
-  validates :shipping_days_id,   presence: true
 
+  VALID_PRICE_REGEX = /\A([1-9]\d*,)*[1-9]\d*\z/.freeze
+  validates :price, presence: true, format: { with: VALID_PRICE_REGEX },
+                    numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 300,
+                                    less_than_or_equal_to: 9_999_999 }
 
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id,        numericality: { other_than: 1 }
-  validates :status_id,          numericality: { other_than: 1 }
-  validates :shipping_fee_id,    numericality: { other_than: 1 }
-  validates :shipping_region_id, numericality: { other_than: 0 }
-  validates :shipping_days_id,   numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :user
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_region_id
+    validates :shipping_days_id
+  end
 
+  # ジャンルの選択が「--」の時は保存できないようにする
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_region_id
+    validates :shipping_days_id
+  end
 
   has_one :order
   belongs_to :user
   has_many_attached :image
-
 end
