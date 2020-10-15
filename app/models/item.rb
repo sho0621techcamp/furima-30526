d596421691ddccd993cdf1ae225b3e0875216a33
sho0621techcamp <sho6217@gmail.com>
2020-10-15 19:51:44 +0900

    Itemモデルのwith_options修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,15 +10,6 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :text, length: { maximum: 1000 }
-  end
-
-  VALID_PRICE_REGEX = /\A([1-9]\d*,)*[1-9]\d*\z/.freeze
-  validates :price, presence: true, format: { with: VALID_PRICE_REGEX },
-                    numericality: { only_integer: true,
-                                    greater_than_or_equal_to: 300,
-                                    less_than_or_equal_to: 9_999_999 }
-
-  with_options presence: true do
     validates :user
     validates :category_id
     validates :status_id
@@ -26,6 +17,12 @@ class Item < ApplicationRecord
     validates :shipping_region_id
     validates :shipping_days_id
   end
+
+  VALID_PRICE_REGEX = /\A([1-9]\d*,)*[1-9]\d*\z/.freeze
+  validates :price, presence: true, format: { with: VALID_PRICE_REGEX },
+                    numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 300,
+                                    less_than_or_equal_to: 9_999_999 }
 
   # ジャンルの選択が「--」の時は保存できないようにする
   with_options presence: true, numericality: { other_than: 1 } do
