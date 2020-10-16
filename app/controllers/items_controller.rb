e370b023ffb689512d6d897675fe4871cb518a20
sho0621techcamp <sho6217@gmail.com>
2020-10-16 20:36:51 +0900

    詳細ページ開通、商品表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,11 +18,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
     params.require(:item).permit(
-      :image, :name, :text, :category_id, :status_id, :shipping_fee_id, :shipping_region_id, :shipping_days_id, :price
+      :image, :name, :text, :category_id, :status_id, :shipping_fee_id, :shipping_region_id, :shipping_day_id, :price
     )
           .merge(user_id: current_user.id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_101201) do
     t.integer "status_id", null: false
     t.integer "shipping_fee_id", null: false
     t.integer "shipping_region_id", null: false
-    t.integer "shipping_days_id", null: false
+    t.integer "shipping_day_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
