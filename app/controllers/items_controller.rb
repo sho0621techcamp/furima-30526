1fe27e9020d4244ad107416cd061df9ea1af8d1e
sho0621techcamp <sho6217@gmail.com>
2020-10-17 14:50:59 +0900

    処理の条件変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order('created_at DESC')
