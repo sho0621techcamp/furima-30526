4e3efc19a0486d2ca232717ca7ad9a872ce8537d
sho0621techcamp <sho6217@gmail.com>
2020-10-17 19:09:47 +0900

    destroyアクションに条件追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show, :new, :create]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,8 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if (user_signed_in? && current_user.id == @item.user_id) && @item.destroy
       redirect_to root_path
     end
   end
