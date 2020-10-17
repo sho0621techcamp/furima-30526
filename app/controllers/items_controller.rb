bf33dd3bcf46669aa3ff65423254f7774d2eaba8
sho0621techcamp <sho6217@gmail.com>
2020-10-17 16:24:12 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
