faa0bcdc1eda0a1ff4cac6b327ae5ee1f05a09e0
sho0621techcamp <sho6217@gmail.com>
2020-10-20 18:32:18 +0900

    テスト追加
    コードレビューで
     <% elsif @item.order == nil && current_user.id == @item.user_id %>
    のように修正するよう指摘ありましたが、修正すると購入画面に進むボタンが出てこなくなるので戻しました。

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -62,5 +62,11 @@ RSpec.describe OrderAddress, type: :model do
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include('Phone number is invalid.')
     end
+
+    it 'phone_numberにハイフンが含まれていると購入できない' do
+      @order_address.phone_number = '090-1234-5678'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number is invalid.")
+    end
   end
 end
