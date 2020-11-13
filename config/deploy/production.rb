944ab8d0ed1ce18449042f49e296e51cbf0eb5ba
sho0621techcamp <sho6217@gmail.com>
2020-11-13 16:22:43 +0900

    ElasticIP変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.249.133.121', user: 'ec2-user', roles: %w{app db web}
+server '18.178.237.103', user: 'ec2-user', roles: %w{app db web}
